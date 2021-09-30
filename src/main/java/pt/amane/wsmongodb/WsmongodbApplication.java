a3adf9b0941a71ba0e5d7c31702e3b9bc1f0856d
Ansumane Mané <manoansu@gmail.com>
2021-09-30 17:18:39 +0100

    project created

---
+++ src/main/java/pt/amane/wsmongodb/WsmongodbApplication.java
@@ -0,0 +1,13 @@
+package pt.amane.wsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/amane/wsmongodb/WsmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.wsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
