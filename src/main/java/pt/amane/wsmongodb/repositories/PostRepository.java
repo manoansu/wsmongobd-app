2eb725fd3e0f3b79fe875412cefc3bdf206ac270
Ansumane Mané <manoansu@gmail.com>
2021-10-01 17:16:14 +0100

    GET Post by title, query operators

--- src/main/java/pt/amane/wsmongodb/repositories/PostRepository.java
+++ src/main/java/pt/amane/wsmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package pt.amane.wsmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pt.amane.wsmongodb.models.entities.Post;
@@ -10,6 +11,9 @@ import pt.amane.wsmongodb.models.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> serachTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
